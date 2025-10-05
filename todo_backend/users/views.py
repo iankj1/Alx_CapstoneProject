55e4d8786c343548df590c1ca913fe7ef96cde3e
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 23:53:25 +0300

    Update views.py

--- todo_backend/users/views.py
+++ todo_backend/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# users/views.py
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
