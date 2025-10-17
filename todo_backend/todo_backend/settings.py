bdd22d93e6bde87d2143bca1b2c9b55f16ce3fb2
Iankj <indumia2@gmail.com>
2025-10-17 23:57:50 +0300

    Final Capstone Project: API + JWT + Swagger completed

--- todo_backend/todo_backend/settings.py
+++ todo_backend/todo_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-secret")
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "False"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 INSTALLED_APPS = [
